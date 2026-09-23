095e8ab5947fadea85e6a1e89dbcf8cc246eb5fa
Yamo <diahnet@naver.com>
2026-09-23 23:06:01 +0900

    chore: bump version to 0.1.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yamone.english"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.1.12"
+        versionCode = 14
+        versionName = "0.1.13"
     }
 
     compileOptions {
