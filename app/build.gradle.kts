032bbd5fea1621282d3491f8bf321af9a0678430
Yamo <diahnet@naver.com>
2026-09-24 00:16:27 +0900

    chore: bump version to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yamone.english"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.2.1"
+        versionCode = 18
+        versionName = "0.2.2"
     }
 
     compileOptions {
